a18da48b35e15ae5c6e42b7292b0b6c7ed1990dc
Huakun Shen <huakun.shen@huakunshen.com>
2023-01-07 22:39:22 -0500

    Switch to sveltekit tauri starter template

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,12 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import staticAdapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: [vitePreprocess()],
+  kit: {
+    adapter: staticAdapter(),
+  },
+};
+
+export default config;
