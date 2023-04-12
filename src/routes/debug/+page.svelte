5abc5cb7813ed89553a227863dd4e52a290413f7
Huakun Shen <huakun.shen@huakunshen.com>
2023-04-12 19:51:25 -0400

    Remove unnecessary permissions from allowlist

--- src/routes/debug/+page.svelte
+++ src/routes/debug/+page.svelte
@@ -1,21 +1,12 @@
 <script lang="ts">
   import { Button } from 'flowbite-svelte';
-  import { configDir } from '@tauri-apps/api/path';
   import { Store } from 'tauri-plugin-store-api';
   import { notify } from '$lib/notify';
-  import {
-    isPermissionGranted,
-    requestPermission,
-    sendNotification,
-  } from '@tauri-apps/api/notification';
-  import { readText, readImage } from 'tauri-plugin-clipboard-api';
+  import { readImage } from 'tauri-plugin-clipboard-api';
   const store = new Store('settings-debug.json');
   
   async function debug() {
     await notify('Hello');
-    // readText().then((text) => {
-    //   console.log(text);
-    // });
     readImage().then((data) => {
       console.log(data);
     });
